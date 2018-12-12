13a504a812f8e911b96a8e4ed3a279ac4868af67
zhg <2414351404@qq.com>
2018-12-12 15:20:44 +0800

    simplify baseline model

--- config.py
+++ config.py
@@ -1,43 +1,41 @@
 class Config(object):
-	def __init__(self):
-		# data file
-		self.train_annFile = r'~\data\vqa\v2_mscoco_train2014_annotations.json'
-		self.train_questionFile = r'~\data\vqa\v2_OpenEnded_mscoco_train2014_questions.json'
-		self.val_annFile = r'~\data\vqa\v2_mscoco_val2014_annotations.json'
-		self.val_questionFile = r'~\data\vqa\v2_OpenEnded_mscoco_val2014_questions.json'
-		self.train_img_dir = r'~\data\vqa\train2014\COCO_train2014_'
-		self.val_img_dir = r'~\data\vqa\val2014\COCO_val2014_'
+    def __init__(self):
+        # data file
+        self.train_annFile = r'../data/v2_mscoco_train2014_annotations.json'
+        self.train_questionFile = r'../datav2_OpenEnded_mscoco_train2014_questions.json'
+        self.val_annFile = r'../data/v2_mscoco_val2014_annotations.json'
+        self.val_questionFile = r'../data/v2_OpenEnded_mscoco_val2014_questions.json'
+        self.train_img_dir = r'../data/train2014/COCO_train2014_'
+        self.val_img_dir = r'../data/val2014/COCO_val2014_'
 
-		# filter condition
-		self.answer_type = None
-		# {'other', 'number', 'yes/no'}
-		self.question_type = None
-		# {'what is on the', 'are the', 'is it', 'is the person', 'can you', 'what is the name', 'how many people are',
-		#  'what are', 'how many people are in', 'is the man', 'do you', 'is this', 'are they', 'is he', 'why is the',
-		#  'where are the', 'does the', 'is there', 'could', 'which', 'is that a', 'where is the', 'what are the', 'why',
-		#  'what room is', 'are', 'none of the above', 'what is', 'what is the woman', 'what color is', 'is the woman',
-		#  'has', 'what is the color of the', 'is this person', 'what number is', 'how many', 'is this a', 'what type of',
-		#  'what animal is', 'does this', 'are these', 'how', 'what is in the', 'are there', 'what is the man', 'is the',
-		#  'what does the', 'who is', 'what is the', 'what color', 'do', 'what brand', 'what', 'what color is the',
-		#  'are there any', 'was', 'is this an', 'what is the person', 'is there a', 'what color are the', 'what kind of',
-		#  'what time', 'is', 'what sport is', 'what is this'}
+        # filter condition
+        self.answer_type = 'yes/no'
+        # {'other', 'number', 'yes/no'}
+        self.question_type = None
+        # {'what is on the', 'are the', 'is it', 'is the person', 'can you', 'what is the name', 'how many people are',
+        #  'what are', 'how many people are in', 'is the man', 'do you', 'is this', 'are they', 'is he', 'why is the',
+        #  'where are the', 'does the', 'is there', 'could', 'which', 'is that a', 'where is the', 'what are the', 'why',
+        #  'what room is', 'are', 'none of the above', 'what is', 'what is the woman', 'what color is', 'is the woman',
+        #  'has', 'what is the color of the', 'is this person', 'what number is', 'how many', 'is this a', 'what type of',
+        #  'what animal is', 'does this', 'are these', 'how', 'what is in the', 'are there', 'what is the man', 'is the',
+        #  'what does the', 'who is', 'what is the', 'what color', 'do', 'what brand', 'what', 'what color is the',
+        #  'are there any', 'was', 'is this an', 'what is the person', 'is there a', 'what color are the', 'what kind of',
+        #  'what time', 'is', 'what sport is', 'what is this'}
 
-		# data save directory
-		self.train_data_file = r"~\data\vqa\train_data.csv"
-		self.val_data_file = r"~\data\vqa\val_data.csv"
-		self.vocab2index_dict = r'~\data\vqa\w2i.txt'
-		self.index2vocab_dict = r'~\data\vqa\i2w.txt'
+        # data save directory
+        self.train_data_file = r"../data/train_data.csv"
+        self.val_data_file = r"../data/val_data.csv"
+        self.vocab2index_dict = r'../data/w2i.txt'
+        self.index2vocab_dict = r'../data/i2w.txt'
 
-		self.save_data_file = r'~\data\vqa\data.pkl'
-		self.train_image_feature_dir = r'~\data\vqa\train_image_feature'
-		self.val_image_feature_dir = r'~\data\vqa\val_image_feature'
+        self.save_data_file = r'../data/data.pkl'
+        self.train_image_feature_dir = r'../data/train_image_feature'
+        self.val_image_feature_dir = r'../data/val_image_feature'
 
-		self.base_train_image_feature_dir = r'D:\visualqa\train\base_train_image_feature'
-		self.base_val_image_feature_dir = r'D:\visualqa\val\base_val_image_feature'
-		# answer encode format
-		self.answer_encode_format = 'softmax'
+        # answer encode format
+        self.answer_encode_format = 'softmax'
 
-		self.batch_size = 16
+        self.batch_size = 8
 
-		# data type: train or val
-		self.data_type = 'train'
+        # data type: train or val
+        self.data_type = 'train'
--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import keras.backend as K
-from keras import Input, optimizers, callbacks
+from keras import Input
 from keras.layers import GRU, Bidirectional, Dropout, Dense, Embedding, Lambda, concatenate, Reshape, Softmax
 from keras.models import Model
 from config import Config
@@ -7,119 +7,100 @@ from data_generater import DataGenerate
 
 
 def visual_question_feature_embedding(visual_feature, question_feature, embedding_dim=100):
-	"""
-	:param question_feature:
-	:param visual_feature: output of snack_reshape
-	:param embedding_size:
-	:return:
-	"""
-	gru_hidding_size = 512
-	q = Embedding(input_dim=vocab_size + 1, output_dim=embedding_dim, input_length=query_maxlen)(
-		question_feature)  # (samples, query_maxlen, embedding_dim)
-	q = Dropout(0.5)(q)
-	q_encoder = Bidirectional(GRU(gru_hidding_size), merge_mode='ave')(q)
+    """
+    :param question_feature:
+    :param visual_feature: output of snack_reshape
+    :param embedding_size:
+    :return:
+    """
+    gru_hidding_size = 256
+    q = Embedding(input_dim=vocab_size + 1, output_dim=embedding_dim, input_length=query_maxlen)(
+        question_feature)  # (samples, query_maxlen, embedding_dim)
+    q = Dropout(0.5)(q)
+    q_encoder = Bidirectional(GRU(gru_hidding_size), merge_mode='ave')(q)
 
-	v = Dense(embedding_dim, activation='tanh')(visual_feature)
-	v = Dropout(0.5)(v)
-	v_encoder = Bidirectional(
-		GRU(units=gru_hidding_size, return_sequences=True), merge_mode='ave')(v)
-	return v_encoder, q_encoder
+    v = Dense(embedding_dim, activation='tanh')(visual_feature)
+    v = Dropout(0.5)(v)
+    v_encoder = Bidirectional(
+        GRU(units=gru_hidding_size, return_sequences=True), merge_mode='ave')(v)
+    return v_encoder, q_encoder
 
 
 def gate(inputs):
-	v_encoder, q_encoder, pre_memory = inputs
-	z = K.concatenate([v_encoder * Reshape((1, 512))(q_encoder), v_encoder * Reshape((1, 512))(pre_memory),
-					   K.abs(v_encoder - Reshape((1, 512))(q_encoder)),
-					   K.abs(v_encoder - Reshape((1, 512))(pre_memory))], axis=-1)
-	return z
+    v_encoder, q_encoder, pre_memory = inputs
+    z = K.concatenate([v_encoder * Reshape((1, 256))(q_encoder), v_encoder * Reshape((1, 256))(pre_memory),
+                       K.abs(v_encoder - Reshape((1, 256))(q_encoder)),
+                       K.abs(v_encoder - Reshape((1, 256))(pre_memory))], axis=-1)
+    return z
 
 
 def softmax_norm(input):
-	return K.exp(input) / K.sum(K.exp(input))
+    return K.exp(input) / K.sum(K.exp(input))
 
 
 def episodic_memory_module(visual_feature, question_feature, pre_memory):
-	"""
-	soft attention
-	:param visual_feature:  shape: (batch_size, 14*14(local_size) , embedding_size)
-	:param question_feature: shape: (batch_size, embedding_size)
-	:param pre_memory: shape: (batch_size, embeding_size)
-	:return:
-	"""
-	# calculate update gate z
-	z = Lambda(gate)([visual_feature, question_feature, pre_memory])
-	z = Dense(128, activation='tanh')(z)
-	z = Dropout(0.5)(z)
-	# softmax_size = visual_feature.shape[1]  # softmax_size = embdding_size
-	z = Dense(1)(z)  # shape: (batch_size, 196)
-	z = Lambda(softmax_norm)(z)
-	c = Lambda(lambda inputs: K.sum(inputs[0] * inputs[1], axis=1))([visual_feature, z])
-	cur_memory = Dense(512, activation='relu')(concatenate([pre_memory, c, question_feature], axis=-1))
-	cur_memory = Dropout(0.5)(cur_memory)
-	return cur_memory
+    """
+    soft attention
+    :param visual_feature:  shape: (batch_size, 14*14(local_size) , embedding_size)
+    :param question_feature: shape: (batch_size, embedding_size)
+    :param pre_memory: shape: (batch_size, embeding_size)
+    :return:
+    """
+    # calculate update gate z
+    z = Lambda(gate)([visual_feature, question_feature, pre_memory])
+    z = Dense(128, activation='tanh')(z)
+    z = Dropout(0.5)(z)
+    # softmax_size = visual_feature.shape[1]  # softmax_size = embdding_size
+    z = Dense(1)(z)  # shape: (batch_size, 196)
+    z = Lambda(softmax_norm)(z)
+    c = Lambda(lambda inputs: K.sum(inputs[0] * inputs[1], axis=1))([visual_feature, z])
+    cur_memory = Dense(256, activation='relu')(concatenate([pre_memory, c, question_feature], axis=-1))
+    cur_memory = Dropout(0.5)(cur_memory)
+    return cur_memory
 
 
 def answer_module(memory, question, answer_size):
-	answer = Dense(128, activation='relu')(concatenate([memory, question]))
-	answer = Dense(1, activation='sigmoid')(answer)
-	return answer
+    answer = Dense(128, activation='relu')(concatenate([memory, question]))
+    answer = Dense(1, activation='sigmoid')(answer)
+    return answer
 
 
-def build_model(input_v, input_q, answer_size):
-	"""
-	build and complie model
-	:param input_v: visual local feature with shape of (14*14, 512)
-	:param input_q: question encoded with vector
-	:return: model
-	"""
-	v_encoder, q_encoder = visual_question_feature_embedding(input_v, input_q)
-	cur_memory = episodic_memory_module(v_encoder, q_encoder, q_encoder)
-	cur_memory = episodic_memory_module(v_encoder, q_encoder, cur_memory)
-	output = answer_module(cur_memory, input_q, answer_size)
+def build_model(input_v, input_q):
+    """
+    build and complie model
+    :param input_v: visual local feature with shape of (14*14, 512)
+    :param input_q: question encoded with vector
+    :return: model
+    """
+    v_encoder, q_encoder = visual_question_feature_embedding(input_v, input_q)
+    cur_memory = episodic_memory_module(v_encoder, q_encoder, q_encoder)
+    cur_memory = episodic_memory_module(v_encoder, q_encoder, cur_memory)
+    output = answer_module(cur_memory, input_q, answer_size)
 
-	model = Model([input_v, input_q], output)
+    model = Model([input_v, input_q], output)
 
-	opt = optimizers.Adam(lr=0.0001)
-
-	model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
-	return model
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model
 
 
 if __name__ == '__main__':
-	config = Config()
-	data = DataGenerate(config)
-	train = data.generate_data()
+    config = Config()
+    data = DataGenerate(config)
+    train = data.generate_data()
 
-	setting = data.get_config()
-	answer_size = setting['answer_word_size'] - 1
-	steps_per_epoch = setting['steps_per_epoch']
-	vocab_size = setting['vocab_size']
-	query_maxlen = setting['max_question_size']
+    setting = data.get_config()
+    answer_size = setting['answer_word_size'] - 1
+    steps_per_epoch = setting['steps_per_epoch']
+    vocab_size = setting['vocab_size']
+    query_maxlen = setting['max_question_size']
 
-	input_v = Input(shape=(14 * 14, 512))
-	input_q = Input(shape=(query_maxlen,))
-	# data._encode_image()
+    input_v = Input(shape=(14 * 14, 512))
+    input_q = Input(shape=(query_maxlen,))
+    # data._encode_image()
 
-	model = build_model(input_v, input_q, answer_size)
-	print(model.summary())
+    model = build_model(input_v, input_q)
+    print(model.summary())
 
-	model_name = 'my_model.h5'
-	modelCheckpoint = callbacks.ModelCheckpoint(
-		model_name,
-		monitor='val_loss',
-		verbose=1,
-		save_best_only=True,
-		save_weights_only=False,
-		mode='auto',
-		period=1)
+    model.fit_generator(train, steps_per_epoch=steps_per_epoch, epochs=2)
 
-	earlyStopping = callbacks.EarlyStopping(
-		monitor='val_loss',
-		patience=10,
-		verbose=1)
-
-	model.fit_generator(train, steps_per_epoch=steps_per_epoch, epochs=2, class_weight='auto',
-				  callbacks=[modelCheckpoint,
-							 earlyStopping])
-
-	model.save('model.h5')
+    model.save('model.h5')
